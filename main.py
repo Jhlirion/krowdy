6aae12608b82b84b3ad38a90df4ec96a0cf29747
Lirion <Helton.Lirion@reprisedigital.com>
2023-04-13 11:37:21 -0500

    menu

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from tratamiento import trat_carg, dic_json
 
-trat_carg()
-dic_json()
 
+if __name__ == '__main__':
+    
+    #Homologación de datos de personas registradas
+    trat_carg()
 
-
+    #generacion de sinonimos de universidades
+    dic_json()
 
